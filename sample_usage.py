af29bb6cb40b6e98ef20ef1cf46ce251093786b5
ahmetozdemir1071 <33777806+ahmetozdemir1071@users.noreply.github.com>
2021-03-07 23:31:23 +0300

    Add files via upload

---
+++ sample_usage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  7 22:32:46 2021
+
+@author: ahmet
+"""
+
+import cv2
+import numpy as np
+import auto_canny as ac
+
+camera=cv2.VideoCapture(0) #webcamm açmak için 0 yazdık
+
+while True:
+    ret,image=camera.read()
+    img_blur=cv2.blur(image,(2,2)) #gürültü azaltmak için
+    edges = ac.canny_smart(img_blur,0.33)
+    cv2.imshow("edges",edges)
+    
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+    
+camera.release()
+cv2.destroyAllWindows()
